21ce7657f86cdd9dbd237b49998e417718341ba3
Phoung monyrak <monyrak0166@gamil.com>
2026-09-21 23:06:53 +0700

    Add message handling to ATM exception

--- ATMException.java
+++ ATMException.java
@@ -1,4 +1,11 @@
 package atm;
 
 public class ATMException extends Exception {
+    private final String message;
+
+    public ATMException(String message) {
+        super(message);
+        this.message = message;
+    }
+    
 }
